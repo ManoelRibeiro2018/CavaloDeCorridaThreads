d82fd6bbfc574e73f7fbfd8ef61e646054c87d11
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2021-12-07 21:12:16 -0300

    Adicionando Sempaforo

---
+++ CavaloDeCorrida/Cavalo/CavaloModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CavaloDeCorrida.Cavalo
+{
+    public class CavaloModel
+    {
+        public int Id { get; set; }
+        public static PictureBox  CavaloOne { get; internal set; }
+        public static PictureBox CavaloTwo { get; internal set; }
+        public static PictureBox CavaloThree { get; internal set; }
+        public Point CavaloPointOne { get; set; }
+        public Point CavaloPointTwo { get; internal set; }
+        public Point CavaloPointThree { get; internal set; }
+
+        public CavaloModel(PictureBox cavaloOne, PictureBox cavaloTwo, PictureBox cavaloThree)
+        {
+            CavaloOne = cavaloOne;
+            CavaloTwo = cavaloTwo;
+            CavaloThree = cavaloThree;
+        }
+     
+    }
+}
--- CavaloDeCorrida/Form1.cs
+++ CavaloDeCorrida/Form1.cs
@@ -1,69 +1,113 @@
-﻿using System;
+﻿using CavaloDeCorrida.Cavalo;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Drawing;
 using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CavaloDeCorrida
 {
     public partial class Form1 : Form
     {
         public PictureBox MeuPictureBox = null;
-        public Random  Random { get; set; }
-        public int  Chegada { get; set; }
+        public int Finish { get; set; }
+
+        SemaphoreSlim semaphore = new(2);
+        public List<CavaloModel> CavaloModels { get; set; }
+
+        public int chegada = 0;
 
         public Form1()
         {
             InitializeComponent();
             CheckForIllegalCrossThreadCalls = false;
-            Random = new Random();
-            Chegada = 1400;
+            Finish = 932;
+            new CavaloModel(pictureBox1, pictureBox2, pictureBox3);
         }
 
         private void StartBtn_Click(object sender, EventArgs e)
         {
             var thread = new List<Task>();
-            thread.Add(Task.Factory.StartNew(Metodo));
-            thread.Add(Task.Factory.StartNew(Metodo));
-            thread.Add(Task.Factory.StartNew(Metodo));
-        }
-        private void Metodo()
-        {
-            var cavalo1 = pictureBox1;
-            var cavalo2 = pictureBox2;
-            var cavalo3 = pictureBox3;
 
-            Point jogador1 = cavalo1.Location;
-            Point jogador2 = cavalo2.Location;
-            Point jogador3 = cavalo3.Location;
-
-            for (int i = 0; i < 1040; i++)
+            for (int i = 0; i < 3; i++)
             {
-                Thread.Sleep(Random.Next(0,20));
-
-                if (Task.CurrentId == 1)
-                {
-                    jogador1.X = i;
-                    cavalo1.Location = jogador1;
-                }
-                if (Task.CurrentId == 2)
-                {
-                    jogador2.X = i;
-                    cavalo2.Location = jogador2;
-                }
-                if (Task.CurrentId == 3)
-                {
-                    jogador3.X = i * i;
-                    cavalo3.Location = jogador3;
-                }
+                thread.Add(Task.Factory.StartNew(PrepareRace));
             }
-        }        
-       
+        }
+        private void PrepareRace()
+        {
+            for (int length = 0; length <= Finish; length++)
+            {
+                Run(length);
+            }
+        }
+
+        private void Run(int length)
+        {
+            semaphore.Wait();
+
+            Point jogador1 = CavaloModel.CavaloOne.Location;
+            Point jogador2 = CavaloModel.CavaloTwo.Location;
+            Point jogador3 = CavaloModel.CavaloThree.Location;
+
+
+            //Thread.Sleep(1);
+
+            if (Task.CurrentId == 1)
+            {
+                jogador1.X = length;
+                CavaloModel.CavaloOne.Location = jogador1;
+            }
+            if (Task.CurrentId == 2)
+            {
+                jogador2.X = length;
+                CavaloModel.CavaloTwo.Location = jogador2;
+            }
+            if (Task.CurrentId == 3)
+            {
+                jogador3.X = length;
+                CavaloModel.CavaloThree.Location = jogador3;
+            }
+
+            //FinishRace(Task.CurrentId, length);
+
+            semaphore.Release();
+        }
+
+        void Wait()
+        {
+            if (NvlList.SelectedItem.ToString() == "Ativado")
+            {
+                semaphore.Wait();
+            }
+        }
+
+        void Release()
+        {
+            if (NvlList.SelectedItem.ToString() == "Ativado")
+            {
+                semaphore.Release();
+            }
+        }
+
+        public void FinishRace(int? id, int length)
+        {
+
+            if (length == Finish && chegada == 0)
+            {
+                MessageBox.Show($"Cavalo: {id} é o vencedor!!! ");
+                ++chegada;
+            }
+            else if (length == Finish && chegada == 1)
+            {
+                MessageBox.Show($"Cavalo: {id} chegou em segundo lugar!!! ");
+                ++chegada;
+            }
+            else if (length == Finish && chegada == 2)
+            {
+                MessageBox.Show($"Cavalo: {id} chegou em terceiro lugar!!! ");
+            }
+        }
     }
 }
